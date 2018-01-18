b5208bed4e2e30ea7209f9c1371b528738222fec
Alejandro Salgueiro <alejandro.salgueiro@flyering.com.ar>
2018-01-18 10:36:05 -0300

    FIX: Error en el comentario con la constante de producción MEJORA: Se agregó el catch de las excepciones en el llamado principal

--- api/obtener_pagos.php
+++ api/obtener_pagos.php
@@ -16,33 +16,39 @@ $epagos = new epagos_api(ID_ORGANISMO, ID_USUARIO);
 //
 // el SDK soporta dos entornos:
 // Testing    -> EPAGOS_ENTORNO_SANDBOX
-// Producción -> EPAGOS_ENTORNO_PRODUCCIOM
+// Producción -> EPAGOS_ENTORNO_PRODUCCION
 //
-$epagos->set_entorno(EPAGOS_ENTORNO_SANDBOX);
-$ret = $epagos->obtener_token(PASSWORD, HASH);
-if (!$ret["token"]){
-  echo 'Error: <b>generar_token</b>: ';
+try {
+  $epagos->set_entorno(EPAGOS_ENTORNO_SANDBOX);
+  $ret = $epagos->obtener_token(PASSWORD, HASH);
+  if (!$ret["token"]) {
+    echo 'Error: <b>generar_token</b>: ';
+    echo "<pre>";
+    print_r($ret);
+    echo "</pre>";
+    exit;
+  }
+
+  //
+  // consulta la documentación para ver los criterios de búsqueda posibles
+  //
+  $criterios = [];
+  $entidades = $epagos->obtener_entidades_pago($criterios);
+  echo "Resultado: <b>obtener_entidades_pago</b>";
   echo "<pre>";
-  print_r($ret);
+  print_r($entidades);
+
+  //
+  // consulta la documentación para ver los criterios de búsqueda posibles
+  //
+  $criterios = ["Estado" => "A"];
   echo "</pre>";
-  exit;
-}
+  $pagos = $epagos->obtener_pagos($criterios);
+  echo "Resultado: <b>obtener_pagos</b>";
+  echo "<pre>";
+  print_r($pagos);
+  echo "</pre>";
 
-//
-// consulta la documentación para ver los criterios de búsqueda posibles
-//
-$criterios = [];
-$entidades = $epagos->obtener_entidades_pago($criterios);
-echo "Resultado: <b>obtener_entidades_pago</b>";
-echo "<pre>";
-print_r($entidades);
-
-//
-// consulta la documentación para ver los criterios de búsqueda posibles
-//
-$criterios = ["Estado" => "A"];
-echo "</pre>";$pagos = $epagos->obtener_pagos($criterios);
-echo "Resultado: <b>obtener_pagos</b>";
-echo "<pre>";
-print_r($pagos);
-echo "</pre>";
+} catch (EPagos_Exception $e){
+  echo "Error: ".$e->getMessage();
+} /* try, catch */
--- post/inicio.php
+++ post/inicio.php
@@ -27,58 +27,63 @@ $epagos = new epagos_api(ID_ORGANISMO, ID_USUARIO);
 //
 // el SDK soporta dos entornos:
 // Testing    -> EPAGOS_ENTORNO_SANDBOX
-// Producción -> EPAGOS_ENTORNO_PRODUCCIOM
+// Producción -> EPAGOS_ENTORNO_PRODUCCION
 //
-$epagos->set_entorno(EPAGOS_ENTORNO_SANDBOX);
-$respuesta = $epagos->obtener_token_post(PASSWORD, HASH);
-// control de error en la respuesta
-if (empty($respuesta->token)){
-  echo 'Error: <b>obtener_token_post</b>: ';
-  echo "<pre>";
-  print_r($respuesta);
-  echo '</pre>';
-  exit;
-}
+try {
+  $epagos->set_entorno(EPAGOS_ENTORNO_SANDBOX);
+  $respuesta = $epagos->obtener_token_post(PASSWORD, HASH);
+  // control de error en la respuesta
+  if (empty($respuesta->token)) {
+    echo 'Error: <b>obtener_token_post</b>: ';
+    echo "<pre>";
+    print_r($respuesta);
+    echo '</pre>';
+    exit;
+  }
 
-$token = $respuesta->token;
+  $token = $respuesta->token;
 
 
-//TODO: personalizar con sus valores y detalles a enviar, esto es opcional, puede no enviarlo
-$detalle_op = urlencode(json_encode(
-  [
+  //TODO: personalizar con sus valores y detalles a enviar, esto es opcional, puede no enviarlo
+  $detalle_op = urlencode(json_encode(
     [
-      'id_item'       => '1',
-      'desc_item'     => 'Descripcion 1',
-      'monto_item'    => '120',
-      'cantidad_item' => '1'
-    ],
-    [
-      'id_item'       => '2',
-      'desc_item'     => 'Descripcion 2',
-      'monto_item'    => '120',
-      'cantidad_item' => '3'
-    ],
-  ]
-));
-$datos_post = [
-  'numero_operacion'    => '1',
-  'id_moneda_operacion' => '1',
-  'monto_operacion'     => 480,   // TODO: reemplazar por el importe de la operación
-  'detalle_operacion'   => $detalle_op,
-  //
-  // En el caso de que se desee mostrar el usuario el detalle en la pantalla de pago, incluir:
-  //  'detalle_operacion_visible' => 1,
-  //
-  'ok_url'              => URL_OK,     //TODO: reemplazar por sus URL
-  'error_url'           => URL_ERROR,  //TODO: reemplazar por sus URL
-  //
-  // Usos avanzados
-  //
-  // - Para restringir que solo se vean determinados medios de pago:
-  // $datos_post['fp_permitidas'] = serialize([1, 2, 3, 9, 10, 11]);
-  //
-  // - Para excluir determinados medios de pago:
-  // $datos_post['fp_excluidas'] = serialize([1, 2, 3]);
-  //
-];
-$epagos->solicitud_pago_post($datos_post);
+      [
+        'id_item' => '1',
+        'desc_item' => 'Descripcion 1',
+        'monto_item' => '120',
+        'cantidad_item' => '1'
+      ],
+      [
+        'id_item' => '2',
+        'desc_item' => 'Descripcion 2',
+        'monto_item' => '120',
+        'cantidad_item' => '3'
+      ],
+    ]
+  ));
+  $datos_post = [
+    'numero_operacion' => '1',
+    'id_moneda_operacion' => '1',
+    'monto_operacion' => 480,   // TODO: reemplazar por el importe de la operación
+    'detalle_operacion' => $detalle_op,
+    //
+    // En el caso de que se desee mostrar el usuario el detalle en la pantalla de pago, incluir:
+    //  'detalle_operacion_visible' => 1,
+    //
+    'ok_url' => URL_OK,     //TODO: reemplazar por sus URL
+    'error_url' => URL_ERROR,  //TODO: reemplazar por sus URL
+    //
+    // Usos avanzados
+    //
+    // - Para restringir que solo se vean determinados medios de pago:
+    // $datos_post['fp_permitidas'] = serialize([1, 2, 3, 9, 10, 11]);
+    //
+    // - Para excluir determinados medios de pago:
+    // $datos_post['fp_excluidas'] = serialize([1, 2, 3]);
+    //
+  ];
+  $epagos->solicitud_pago_post($datos_post);
+
+} catch (EPagos_Exception $e){
+  echo "Error: ".$e->getMessage();
+} /* try, catch */
